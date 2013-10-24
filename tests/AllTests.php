89fcfd5fc36f8116f68eae919a7b59e7844df684
felixaemiliano <felixaemiliano@gmail.com>
2013-10-23 23:49:18 -0400

    Added Gender to Person Edit as well as in the Booking Edit forms

---
+++ tests/AllTests.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Run all the RMH Homeroom unit tests
+ */
+// require_once(dirname(__FILE__).'/simpletest/autorun.php');
+class AllTests extends GroupTest {
+ 	  function AllTests() {
+        $this->addTestFile(dirname(__FILE__).'/testdbPersons.php');
+        $this->addTestFile(dirname(__FILE__).'/testPerson.php');
+        $this->addTestFile(dirname(__FILE__).'/testdbBookings.php');
+        $this->addTestFile(dirname(__FILE__).'/testBooking.php');
+        $this->addTestFile(dirname(__FILE__).'/testdbRooms.php');
+        $this->addTestFile(dirname(__FILE__).'/testRoom.php');
+        $this->addTestFile(dirname(__FILE__).'/testdbRoomLogs.php');
+        $this->addTestFile(dirname(__FILE__).'/testRoomLog.php');
+        $this->addTestFile(dirname(__FILE__).'/testOccupancyData.php');
+       
+        echo ("\nAll tests complete");
+ 	  }
+ }
+?>

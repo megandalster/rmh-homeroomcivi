1dceb65cc198412f90512f45bdaf84ee77cb5037
allen <allen@bowdoin.edu>
2014-03-07 14:09:54 -0500

    added live data import for guests and bookings

---
+++ importbookings.php
@@ -0,0 +1,10 @@
+<?php
+include_once(database/dbBookings.php);
+include_once(domain/Booking.php);
+include_once(database/dbPersons.php);
+include_once(domain/Person.php);
+
+fopen(bookings1_50.csv,"r");
+fgetcsv
+
+?>
